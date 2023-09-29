cec203f83e1358b483df489a92bd70cd84a162ea
Paul Guillen <paulguillen190@gmail.com>
2023-09-28 20:20:49 -0500

    Relizando diseño de Perfil

--- src/screens/home/Perfil.js
+++ src/screens/home/Perfil.js
@@ -1,11 +1,67 @@
-import { View, Text } from "react-native";
+import { Text, Image, StyleSheet, View, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 const Perfil = () => {
+  const navigation = useNavigation();
+  const userProfile = {
+    fullName: "John Doe",
+    email: "johndoe@example.com",
+    profileImage: require("../../../assets/images/eating.png"),
+  };
+
+  const handleLogout = () => {
+    navigation.navigate("Login");
+  };
+
   return (
-    <View>
-      <Text>Perfil</Text>
+    <View style={styles.container}>
+      <Image source={userProfile.profileImage} style={styles.profileImage} />
+      <Text style={styles.fullName}>{userProfile.fullName}</Text>
+      <Text style={styles.email}>{userProfile.email}</Text>
+      <View style={styles.containerBottom}>
+        <TouchableOpacity onPress={handleLogout} style={styles.logoutButton}>
+          <Text style={styles.logoutText}>Salir sesión</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  profileImage: {
+    width: 160,
+    height: 180,
+    borderRadius: 60,
+    marginBottom: 20,
+  },
+  fullName: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  email: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
+  logoutButton: {
+    backgroundColor: "red",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+  },
+  logoutText: {
+    color: "white",
+    fontWeight: "bold",
+  },
+  containerBottom: {
+    position: "absolute",
+    bottom: 40,
+  },
+});
 
 export default Perfil;
