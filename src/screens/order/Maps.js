acf68ea1db0251a335469bcfebef0e9d6917445e
Paul Guillen <paulguillen190@gmail.com>
2023-10-05 23:11:10 -0500

    Abriendo google maps

--- src/screens/order/Maps.js
+++ src/screens/order/Maps.js
@@ -1,9 +1,11 @@
-import { useEffect } from "react";
-import { BackHandler, StyleSheet, View } from "react-native";
+import { useEffect, useState } from "react";
+import { BackHandler, Button, StyleSheet, Text, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-
+import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
 const Maps = () => {
   const navigation = useNavigation();
+  const [location, setLocation] = useState(null);
 
   useEffect(() => {
     const backHandler = BackHandler.addEventListener(
@@ -20,24 +22,42 @@ const Maps = () => {
   }, []);
 
   useEffect(() => {
-    const backHandler = BackHandler.addEventListener(
-      "hardwareBackPress",
-      () => {
-        backNewAddress();
-        return true;
+    (async () => {
+      // Solicitar permisos de ubicación
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permiso de ubicación denegado");
+        return;
       }
-    );
 
-    return () => {
-      backHandler.remove();
-    };
+      // Obtener la ubicación actual
+      const location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
   }, []);
 
   const backNewAddress = () => {
     navigation.navigate("Order", { screen: "CreateAddress" });
   };
 
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <MapView style={styles.map} initialRegion={location}>
+        {location && (
+          <Marker
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
+            title="Ubicación"
+            description="Descripción de la ubicación"
+          />
+        )}
+      </MapView>
+      <Text style={styles.text}>Maps</Text>
+      <Button title="Volver" onPress={backNewAddress} />
+    </View>
+  );
 };
 
 export default Maps;
